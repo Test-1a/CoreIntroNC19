7b338b9956443a393162d0b11c243a703792774d
DZM <d.bjorlingh@gmail.com>
2019-12-03 14:18:40 +0100

    Add project files.

---
+++ CoreIntroNC19/Controllers/DemoController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Encodings.Web;
+using System.Threading.Tasks;
+using CoreIntroNC19.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreIntroNC19.Controllers
+{
+    public class DemoController : Controller
+    {
+        public IActionResult Index(int salary, string name)
+        {
+            //return HtmlEncoder.Default.Encode($"Demo index id:{id} name {name}");
+            //ViewData["id"] = salary;
+            //ViewData["name"] = name;
+
+            var model = new Person()
+            {
+                Name = name,
+                Salary =salary
+            };
+
+            return View(model);
+        }
+    }
+}
---
+++ CoreIntroNC19/Models/Person.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreIntroNC19.Models
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public int Salary { get; set; }
+    }
+}
